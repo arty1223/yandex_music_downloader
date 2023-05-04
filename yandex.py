b4aec98d9b2ced247c1a447a9147a0d6bf524257
arty1223 <ar23skiba12@gmail.com>
2023-05-04 22:51:22 +0300

    first commit

---
+++ yandex.py
@@ -0,0 +1,47 @@
+from yandex_music import Client
+from os import mkdir, chdir, path
+from datetime import datetime
+from token_file import token
+
+try:    
+    client = Client(token).init()    
+except:
+    print("ошибка аутентификации")
+    file = open("token_file.py","w")
+    file.write(f"token = '{input('введите OAuth токен (как получить https://github.com/MarshalX/yandex-music-api/discussions/513#discussioncomment-2729781): ')}'"+'\n')
+    file.close()
+    input('saved, press enter to exit ')
+    exit()
+
+try:
+    chdir('yandex_music')
+except:
+    mkdir('yandex_music')
+    chdir('yandex_music')
+
+
+amount = len(client.users_likes_tracks())
+def fncheck(filename : str):
+    result = filename
+    for i in filename:
+        if i in '<>:"/\|?*':
+           result = result.replace(i,"")     
+    return result
+
+time = datetime.now()
+for i in range(amount):
+    track = client.users_likes_tracks()[i].fetch_track()
+    try:
+        artist = track['artists'][0]['name']
+        filename = fncheck(f"{track['title']} - {artist}.mp3")
+    except:
+        print("no artist")
+        filename = fncheck(f"{track['title']}.mp3")
+    print (f"{filename} curent track {i} from {amount-1}, {round(i/(amount-1)*100,1)}%")
+    if path.exists(filename):
+        print('skip')
+        continue
+    client.users_likes_tracks()[i].fetch_track().download(filename)   
+print(f"""done!
+took {datetime.now() - time}""")
+input()
